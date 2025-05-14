6fc55ed2c6bf34006cefc229b50e7c55ba492955
rowdypiperprogramming <s2005493@online.houstonisd.org>
2025-05-13 20:25:55 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+function twozero() {
+        const twenty = Math.floor(Math.random() * 20) + 1;
+        let box = document.getElementById("box");
+        box.textContent = twenty;
+      }
+
+      function onetwo() {
+        const twelve = Math.floor(Math.random() * 12) + 1;
+        let box = document.getElementById("box");
+        box.textContent = twelve;
+      }
+
+      function oneten() {
+        const ten = Math.floor(Math.random() * 10) + 1;
+        let box = document.getElementById("box");
+        box.textContent = ten;
+      }
+
+      function zeroeight() {
+        const eight = Math.floor(Math.random() * 8) + 1;
+        let box = document.getElementById("box");
+        box.textContent = eight;
+      }
+
+      function zerosix() {
+        const six = Math.floor(Math.random() * 6) + 1;
+        let box = document.getElementById("box");
+        box.textContent = six;
+      }
+
+      function zerofour() {
+        const four = Math.floor(Math.random() * 4) + 1;
+        let box = document.getElementById("box");
+        box.textContent = four;
+      }
